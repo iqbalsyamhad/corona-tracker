85970e3c854721466535b0ef902b30209912d883
shidqi <nauvalholic@gmail.com>
2020-10-17 20:11:46 +0700

    UPDATE: ENG parameter

--- app/helper/number.js
+++ app/helper/number.js
@@ -1,3 +1,3 @@
-module.exports = function(angka) {    
-    return Number(angka).toFixed(0).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
+module.exports = function(number) {    
+    return Number(number).toFixed(0).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
 }
